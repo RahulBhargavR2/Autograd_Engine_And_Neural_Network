92937d77a7bce6789728fa09b65a2580539b0c76
RahulBhargavR2 <rahulbhargavrgk@gmail.com>
2026-03-20 15:45:32 +0530

    Added ReLU feature

--- scalar/Value.py
+++ scalar/Value.py
@@ -9,7 +9,7 @@ class Value:
         self._op = _op
         self.label = label
         self.grad = 0
-        self._backword = lambda :None
+        self._backward = lambda :None
 
     #method to add 2 values
     def __add__(self, other):
@@ -19,11 +19,13 @@ class Value:
         out = Value(self.data + other.data, (self, other), '+')
 
         #method that populates grad to its previous nodes
-        def _backword():
+        def _backward():
             self.grad += 1.0 * out.grad # derivative of sum is evenly populated, multiplied with previous grad(chained)
             other.grad += 1.0 * out.grad # adding grad in case a single var is used multiple times
-        out._backword = _backword
+        out._backward = _backward
         return out
+    def __radd__(self, other):
+        return self + other
 
     # provides  negated value
     def __neg__(self):
@@ -32,15 +34,18 @@ class Value:
     def __sub__(self, other):
         return self + (-other)
 
+    def __rsub__(self, other):
+        return self - other
+
 
 
     def __mul__(self, other):
         other =  other if isinstance(other,Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
-        def _backword():
+        def _backward():
             self.grad += other.data * out.grad # for mul derivative is value of other locally
             other.grad += self.data * out.grad # globally need to multiple with out's grad (chain rule)
-        out._backword = _backword
+        out._backward = _backward
         return out
 
     def __rmul__(self, other):
@@ -51,12 +56,20 @@ class Value:
 
     def tanh(self):
         x = self.data
-        exp = (math.exp(2*x)-1)/(math.exp(2*x)+1) # tanh representation in e
+        # exp = (math.exp(2*x)-1)/(math.exp(2*x)+1) # tanh representation in e
+        exp = math.tanh(x)
         out = Value(exp,(self,),'tanh')
-        def _backword():
+        def _backward():
             self.grad += (1-exp**2 )* out.grad # derivative of tanh is (1-tanh^2)
-        out._backword = _backword
+        out._backward = _backward
 
+        return out
+
+    def relu(self):
+        out =  Value(self.data if self.data > 0 else 0,(self,),'relu')
+        def _backward():
+            self.grad += (out.data>0) * out.grad
+        out._backward = _backward
         return out
 
 
@@ -65,23 +78,23 @@ class Value:
         x = self.data
         out = Value(math.exp(x),(self,),'exp')
 
-        def _backword():
+        def _backward():
             self.grad += out.data * out.grad # derivative of e^x is e^x only, multiply to get grad globally
-        out._backword = _backword
+        out._backward = _backward
         return out
 
     def __pow__(self, other):
         assert isinstance(other,(int,float))
         out = Value(self.data**other,(self,),f'**{other}')
 
-        def _backword():
+        def _backward():
             self.grad += out.data * (self.data**(other-1)) * out.grad # derivative of a^n is n*a^(n-1)
 
-        out._backword = _backword
+        out._backward = _backward
         return out
 
     # method to backpropogate
-    def backword(self):
+    def backward(self):
         topo = []
         visited = set()
 
@@ -96,7 +109,7 @@ class Value:
         self.grad = 1
         #backpropogate to all nodes from right to left
         for node in reversed(topo):
-            node._backword()
+            node._backward()
 
     def __repr__(self):
         return f"Value(data = {self.data})"
@@ -105,22 +118,25 @@ class Value:
 
 
 
+#
+# b = Value(6.881373587,label='b')
+# x1 = Value(2.0,label='x1')
+# x2 = Value(0.0,label='x2')
+#
+# w1 = Value(-3.0,label='w1')
+# w2 = Value(1.0,label='w2')
+#
+# x1w1 = x1 * w1; x1w1.label='x1w1'
+# x2w2 = x2 * w2; x2w2.label='x2w2'
+#
+# x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label='x1w1x2w2'
+# n = x1w1x2w2 +b; n.label='n'
+#
+# o = n.tanh(); o.label='o'
+#
+#
+# o.backward()
+# draw_dot(o).render(view=True)
 
-b = Value(6.881373587,label='b')
-x1 = Value(2.0,label='x1')
-x2 = Value(0.0,label='x2')
-
-w1 = Value(-3.0,label='w1')
-w2 = Value(1.0,label='w2')
-
-x1w1 = x1 * w1; x1w1.label='x1w1'
-x2w2 = x2 * w2; x2w2.label='x2w2'
-
-x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label='x1w1x2w2'
-n = x1w1x2w2 +b; n.label='n'
-
-o = n.tanh(); o.label='o'
 
 
-o.backword()
-draw_dot(o).render(view=True)
