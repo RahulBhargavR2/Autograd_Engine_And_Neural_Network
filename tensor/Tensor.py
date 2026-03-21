2d76eb45748ff4842908fc2c89b6d3932bcc5cf6
RahulBhargavR2 <rahulbhargavrgk@gmail.com>
2026-03-21 20:15:31 +0530

    added broadcasting (repeating the data to match the required data) and then added un broadcast to retrieve the original after performing the needed operation

--- tensor/Tensor.py
+++ tensor/Tensor.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 class Tensor:
     # def __init__(self, x, _children=(), _op='', label='', requires_grad=True):
     #     self.data = np.array(x, dtype=float)
@@ -13,7 +10,7 @@ class Tensor:
     #     self.label = label
 
     def __init__(self, x, _children=(), _op='', label='', requires_grad=True):
-        self.data = [x] if not isinstance(x, list) else x
+        self.data = x
         self.shape = Tensor.get_shape(self.data)
         self.grad = self.zeros_like() if requires_grad else None
         self.children = set(_children)
@@ -21,12 +18,11 @@ class Tensor:
         self.label = label
         self.requires_grad = requires_grad
 
-
-    # def __add__(self, other):
-    #     out = Tensor(self.data + other.data)
+    def __repr__(self):
+        return f"{self.data}"
 
     @staticmethod
-    def get_shape( data):
+    def get_shape(data):
         if isinstance(data, list):
             # if isinstance(data[0], list):
             #     currL = len(data[0])
@@ -38,6 +34,7 @@ class Tensor:
             return (len(data),) + Tensor.get_shape(data[0])
         else:
             return ()
+
     # returns the 1D representation of the tensor
     @staticmethod
     def flatten(data):
@@ -50,27 +47,24 @@ class Tensor:
         return result
 
     @staticmethod
-    def reshape(data,shape):
+    def reshape(data, shape):
         if len(shape) == 0:
             return data[0]
 
         size = shape[0]
         step = len(data) // size
 
-        return [ Tensor.reshape(data[i*step:(i+1*step)],shape[1:]) for i in range(size)]
+        return [Tensor.reshape(data[i * step:(i + 1 * step)], shape[1:]) for i in range(size)]
 
-    def reshape_tensor(self,new_shape):
+    # main reshape method
+    def reshape_tensor(self, new_shape):
         flat = Tensor.flatten(self.data)
         total = 0
         for ele in new_shape:
             total *= ele
         if ele != len(flat):
             raise Exception("Invalid shape")
-        return Tensor.reshape(flat,new_shape)
-
-
-
-
+        return Tensor.reshape(flat, new_shape)
 
     def zeros_like(self):
         shape = self.shape
@@ -79,17 +73,150 @@ class Tensor:
             data = [data] * ele
         return data
 
+    def backward(self):
+        self.grad = Tensor.ones_like(self.data)
+
+        topo = []
+        visited = set()
+
+        def build(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build(child)
+                topo.append(v)
+
+        build(self)
+
+        for node in reversed(topo):
+            node._backward()
+
+    @staticmethod
+    def broadcast_shape(shape1, shape2):
+        result = []
+
+        i, j = len(shape1) - 1, len(shape2) - 1
+
+        while i >= 0 or j >= 0:
+            dim1 = shape1[i] if i >= 0 else 1
+            dim2 = shape2[j] if j >= 0 else 1
+
+            if dim1 == dim2:
+                result.append(shape2[j])
+            elif dim1 == 1:
+                result.append(shape1[i])
+            elif dim2 == 1:
+                result.append(shape2[i])
+            else:
+                raise Exception("Shape not broadcastable")
+
+        return tuple(reversed(result))
+
+    @staticmethod
+    def broadcast_to(data, target_shape):
+        current_shape = Tensor.get_shape(data)
+
+        # prepend 1s to match dims
+        while len(current_shape) < len(target_shape):
+            data = [data]
+            current_shape = (1,) + current_shape
+        print(current_shape, target_shape)
+
+        out = Tensor._broadcast_recursive(data, current_shape, target_shape)
+
+        return out
+
+    @staticmethod
+    def _broadcast_recursive(data, curr_shape, target_shape):
+        if curr_shape == target_shape:
+            return data
+
+        if len(curr_shape) == 1:
+            if curr_shape[0] == 1:
+                return [Tensor._broadcast_recursive(data[0], (), target_shape[1:]) for _ in range(target_shape[0])]
+
+        result = []
+        for i in range(target_shape[0]):
+            idx = 0 if curr_shape[0] == 1 else i
+            result.append(
+                Tensor._broadcast_recursive(
+                    data[idx],
+                    curr_shape[1:],
+                    target_shape[1:]
+                )
+            )
+
+        return result
+
+    @staticmethod
+    def unbroadcast(grad, original_shape):
+        grad_shape = Tensor.get_shape(grad)
+
+        # remove extra dimensions
+        while len(grad_shape) > len(original_shape):
+            grad = Tensor.sum_axis(grad, axis=0)
+            grad_shape = Tensor.get_shape(grad)
+
+        # collapse broadcasted dims
+        for i, dim in enumerate(original_shape):
+            if dim == 1:
+                grad = Tensor.sum_axis(grad, axis=i)
+
+        return grad
+
+    @staticmethod
+    def sum_axis(data, axis):
+        # Base case: axis = 0 → sum current level
+        if axis == 0:
+            if not isinstance(data[0], list):
+                return sum(data)
+
+            result = data[0]
+            for i in range(1, len(data)):
+                result = Tensor.elementwise_add(result, data[i])
+
+            return result
+
+        # Recursive case: go deeper
+        return [Tensor.sum_axis(sub, axis - 1) for sub in data]
+
+    @staticmethod
+    def elementwise_add(a, b):
+        if not isinstance(a, list):
+            return a + b
+
+        return [Tensor.elementwise_add(x, y) for x, y in zip(a, b)]
+
+    def __add__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+
+        # 1. compute broadcast shape
+        target_shape = Tensor.broadcast_shape(self.shape, other.shape)
+
+        # 2. broadcast data
+        a = Tensor.broadcast_to(self.data, target_shape)
+        b = Tensor.broadcast_to(other.data, target_shape)
+
+        # 3. forward computation
+        out_data = Tensor.elementwise_add(a, b)
+        out = Tensor(out_data, (self, other), '+')
+
+        # 4. backward function
+        def _backward():
+            if self.grad is not None:
+                grad_self = Tensor.unbroadcast(out.grad, self.shape)
+                self.grad = Tensor.elementwise_add(self.grad, grad_self)
+
+            if other.grad is not None:
+                grad_other = Tensor.unbroadcast(out.grad, other.shape)
+                other.grad = Tensor.elementwise_add(other.grad, grad_other)
+
+        out._backward = _backward
+        return out
 
 
-
-
-# t = Tensor([
-#     [[[1, 2, 3], [2, 5, 6], [4, 3, 3]], [[1, 2, 3], [2, 5, 6], [4, 3, 3]], [[1, 2, 3], [2, 5, 6], [4, 3,1 ]]],
-#             [[[1, 2, 3], [2, 5, 6], [4, 3,1]], [[1, 2, 3], [2, 5, 6], [4, 3,2 ]], [[1, 2, 3], [2, 5, 6], [4, 3,4 ]]]])
-# print(t.data[0][0][0][0])
-t = Tensor([[1,2,1],[1,2,1],[1,1,2]])
-
+t1 = Tensor([1, 2])
+t2 = Tensor([1, 2])
+print(Tensor.elementwise_add(t1.data, t2.data))
 # print(t.shape)
-# t.zeros_like()
-# print(t.grad)
-print(t.flatten(t.data))
+# t1 = Tensor.broadcast_to([1, 2], (2, 2))
