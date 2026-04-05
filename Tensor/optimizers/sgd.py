11c5f811fd58354871c71716891a589ff5fb0502
RahulBhargavR2 <rahulbhargavrgk@gmail.com>
2026-04-05 11:32:34 +0530

    minor changes to adapt with tensors

--- Tensor/optimizers/sgd.py
+++ Tensor/optimizers/sgd.py
@@ -1,34 +1,26 @@
 from Tensor.tensor.tensor import Tensor
-class Optimizer:
-    def __init__(self,params):
-        self.params = params
+from Tensor.optimizers.optimizer import Optimizer
 
-    def zero_grad(self):
-        for p in self.params:
-            if p.grad is not None:
-                p.grad = Tensor.zeros_like(p)
 
-    def step(self):
-        raise NotImplementedError
 
 
 class SGD(Optimizer):
-    def __init__(self,params,lr=0.01):
+    def __init__(self, params, lr=0.01):
         super().__init__(params)
         self.lr = lr
 
     def step(self):
         for p in self.params:
             if p.requires_grad:
-                p.data -= self.lr * p.grad
+                p.data = Tensor.elementwise_add(p.data, Tensor.elementwise_mul(p.grad.data, -self.lr))
 
 
 class SGD_momentum(Optimizer):
-    def __init__(self,params,lr=0.01,beta=0.9):
+    def __init__(self, params, lr=0.01, beta=0.9):
         super().__init__(params)
         self.lr = lr
         self.beta = beta
-        self.velocity = [p.zeros_like() for p in params]
+        self.velocity = [Tensor.data_like(p.shape, 0) for p in params]
 
     def step(self):
         for i, p in enumerate(self.params):
@@ -42,6 +34,9 @@ class SGD_momentum(Optimizer):
             # smoothed SGD(EMA - based)
             # vt=βvt−1 + (1−β) gt
             # θ = θ−ηvt
-            self.velocity[i] = self.beta * self.velocity[i] + (1 - self.beta) * p.grad
-            p.data -= self.lr * p.grad
+            # self.velocity[i] = self.beta * self.velocity[i] + (1 - self.beta) * p.grad
+            self.velocity[i] = Tensor.elementwise_add(Tensor.elementwise_mul(self.beta, self.velocity[i]),
+                                                           Tensor.elementwise_mul((1 - self.beta), p.grad.data))
+            # p.data -= self.lr * p.grad
 
+            p.data = Tensor.elementwise_add(p.data, Tensor.elementwise_mul(p.grad.data, -self.lr))
